ba60f09d7df29ab0db71fd204be8d50627ed58ee
maopos <mauricioposadamurillo@gmail.com>
2022-03-14 17:45:06 -0500

    Task Model

---
+++ models/Task.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const TaskSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    state: {
+      type: Boolean,
+      default: false,
+    },
+    releaseDate: {
+      type: Date,
+      default: Date.now(),
+    },
+    priority: {
+      type: String,
+      required: true,
+      enum: ["Low", "Medium", "Hight"],
+    },
+    project: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Project",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Task = mongoose.model("Task", TaskSchema);
+export default Task;
